c3d30cc150e1729f0bdfe804dbcbd3e125ec2478
Enrique Henderson <ehenderson@surgeforward.com>
2017-09-01 15:22:24 +0000

    Merged in enrique-com-fixed-hanging-issue (pull request #45)
    Enrique com fixed hanging issue
    
    * sprint 15
    
    * sprint 15 update
    
    * updated to 15
    
    * deleted templates and adjusted tests for sprint 15
    
    * suggested PR fixes
    
    * fixing build timeout
    
    Approved-by: Salvador Velazquez <svelazquez@surgeforward.com>

--- src/test/java/CommTests/Config.java
+++ src/test/java/CommTests/Config.java
@@ -9,7 +9,7 @@ public interface Config {
 
     final String browser            = System.getProperty("browser", "chrome");
     final String host               = System.getProperty("host","browserstack");// "localhost" or browserstack
-    final String browserVersion     = System.getProperty("browserVersion","54");
+    final String browserVersion     = System.getProperty("browserVersion","57");
     final String platform           = System.getProperty("platform","Windows");
     final String os_version         = System.getProperty("os_version","10");
     final String resolution         = System.getProperty("resolution","1920 x 1080");
